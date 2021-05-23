22532ce8d7e26398893cc6db1d2ad0101cb76670
Jhon Yacolca <77031030+xdreamer02@users.noreply.github.com>
2021-05-23 15:57:35 -0500

    delete

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -5,11 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.empresa.entity.Alumno;
@@ -66,6 +69,19 @@ public class AlumnoController {
 
 		}
 		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Alumno> elimina(@PathVariable("id") int idAlumno){
+		System.out.println(">>>Elmina");
+		Optional<Alumno> optAlumno = service.obtienePorId(idAlumno);
+		if(optAlumno .isPresent()) {
+			service.eliminaAlumno(idAlumno);
+			return ResponseEntity.ok(optAlumno.get());
+		}else {
+			System.out.println("elimina  no existe");
+			return ResponseEntity.badRequest().build();
+		}
 	}
 	
 }
