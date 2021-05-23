50823df7030b21e6a3068fd92ea8c82f4996a368
Jhon Yacolca <77031030+xdreamer02@users.noreply.github.com>
2021-05-23 15:53:10 -0500

    upd

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -1,11 +1,13 @@
 package com.empresa.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +48,24 @@ public class AlumnoController {
 	}
 	
 	
-	
+	@PutMapping
+	public ResponseEntity<Alumno> actualiza(@RequestBody Alumno obj){
+		System.out.println(">>>> Crear ");
+		Optional<Alumno> optAlumno = service.obtienePorId(obj.getIdAlumno());
+		if (optAlumno.isPresent()) {
+			Alumno objSalida = service.insertaActualizaAlumno(obj);
+			if (objSalida != null) {
+
+				return ResponseEntity.ok(objSalida);
+			}else {
+				return ResponseEntity.badRequest().build();
+			}	
+		}else {
+			System.out.println(">>>> Crear ");
+			return ResponseEntity.badRequest().build();
+
+		}
+		
+	}
 	
 }
